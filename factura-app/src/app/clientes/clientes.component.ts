01b6d30e0dcf2b7d7a9692b94bc1075554be5995
Juan José Blanco <juan.blanco03@uptc.edu.co>
2020-11-30 13:46:42 -0500

    Proyecto factura-app añadido al repositorio

---
+++ factura-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    { id: 1, nombre: "ANDRES FELIPE", apellido: "GOMEZ TRUJILLO", email: "andres.gomez@gmail.com", createAT: "26/11/2020" },
+    { id: 2, nombre: "JORGE ANDRES", apellido: "LOPEZ PUMAREJO", email: "jorge.lopez@gmail.com", createAT: "26/11/2020" },
+    { id: 3, nombre: "JUAN JOSÉ", apellido: "BLANCO MENDOZA", email: "juan.blanco@gmail.com", createAT: "26/11/2020" },
+    { id: 4, nombre: "FELIPE SANTIAGO", apellido: "VARGAS CARDENAS", email: "felipe.vargas@gmail.com", createAT: "26/11/2020" },
+    { id: 5, nombre: "EDWIN STEVEN", apellido: "GUAYACAN RAVELO", email: "edwin.guayacan@gmail.com", createAT: "26/11/2020" },
+    { id: 6, nombre: "DANIELA LIZETH", apellido: "ROMERO VASQUEZ", email: "daniela.romero@gmail.com", createAT: "26/11/2020" },
+    { id: 7, nombre: "KAREN GISEL", apellido: "RINCON SUAREZ", email: "karen.rincongmail.com", createAT: "26/11/2020" },
+    { id: 8, nombre: "RAFAEL ARMANDO", apellido: "SUAREZ MENDIETA", email: "rafael.suarez@gmail.com", createAT: "26/11/2020" },
+    { id: 9, nombre: "CAROLINA", apellido: "NIETO HURADO", email: "carolina.nieto@gmail.com", createAT: "26/11/2020" },
+    { id: 10, nombre: "LILIANA FERNANDA", apellido: "MENDOZA PATIÑO", email: "liliana.mendoza@gmail.com", createAT: "26/11/2020" },
+    { id: 10, nombre: "JUAN CAMILO", apellido: "SEPULVEDA", email: "juan.sepulveda@gmail.com", createAT: "26/11/2020" }
+  ]
+
+  //constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
