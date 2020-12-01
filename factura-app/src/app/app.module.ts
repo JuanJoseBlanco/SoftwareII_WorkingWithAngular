2b4cbccff7c4f590834f741fe256e87cdba81aa9
Juan José Blanco <juan.blanco03@uptc.edu.co>
2020-12-01 17:51:25 -0500

    Actualización proyecto facturas-app: Adición de json, header, footr...

--- factura-app/src/app/app.module.ts
+++ factura-app/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
--- factura-app/src/app/clientes/clientes.component.ts
+++ factura-app/src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,23 +9,15 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    { id: 1, nombre: "ANDRES FELIPE", apellido: "GOMEZ TRUJILLO", email: "andres.gomez@gmail.com", createAT: "26/11/2020" },
-    { id: 2, nombre: "JORGE ANDRES", apellido: "LOPEZ PUMAREJO", email: "jorge.lopez@gmail.com", createAT: "26/11/2020" },
-    { id: 3, nombre: "JUAN JOSÉ", apellido: "BLANCO MENDOZA", email: "juan.blanco@gmail.com", createAT: "26/11/2020" },
-    { id: 4, nombre: "FELIPE SANTIAGO", apellido: "VARGAS CARDENAS", email: "felipe.vargas@gmail.com", createAT: "26/11/2020" },
-    { id: 5, nombre: "EDWIN STEVEN", apellido: "GUAYACAN RAVELO", email: "edwin.guayacan@gmail.com", createAT: "26/11/2020" },
-    { id: 6, nombre: "DANIELA LIZETH", apellido: "ROMERO VASQUEZ", email: "daniela.romero@gmail.com", createAT: "26/11/2020" },
-    { id: 7, nombre: "KAREN GISEL", apellido: "RINCON SUAREZ", email: "karen.rincongmail.com", createAT: "26/11/2020" },
-    { id: 8, nombre: "RAFAEL ARMANDO", apellido: "SUAREZ MENDIETA", email: "rafael.suarez@gmail.com", createAT: "26/11/2020" },
-    { id: 9, nombre: "CAROLINA", apellido: "NIETO HURADO", email: "carolina.nieto@gmail.com", createAT: "26/11/2020" },
-    { id: 10, nombre: "LILIANA FERNANDA", apellido: "MENDOZA PATIÑO", email: "liliana.mendoza@gmail.com", createAT: "26/11/2020" },
-    { id: 10, nombre: "JUAN CAMILO", apellido: "SEPULVEDA", email: "juan.sepulveda@gmail.com", createAT: "26/11/2020" }
-  ]
-
-  //constructor() { }
+  clientes: Cliente[];
+  habilitar: boolean = true;
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
+  }
+
+  setHabilitar(){
+    this.habilitar = this.habilitar == true ? false : true
   }
 
 }
